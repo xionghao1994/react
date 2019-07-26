89b028ea135d572edaa8f21b552b3f4fb9d52912
xionghao <18736019320@163.com>
2019-07-26 19:29:34 +0800

    第一次提交

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  // 初始化data数据
+ constructor(props){
+    super(props);
+    this.state = {
+    
+      list:[
+        {name:'哈哈张三'},
+        {name:'嘻嘻李四'},
+        {name:'嘻嘻王五'}
+      ],
+      arr:[
+        {name:'哈哈张1'},
+        {name:'嘻嘻李四'},
+        {name:'嘻嘻王五'}
+      ]
+    }
+  }
+ 
+  // 事件
+   btn(){
+    alert('1')
+ }
+  render(){
+    return(
+      <div>
+       {1+1}<br/>
+          <input />
+          <button onClick={this.btn}>点我</button>
+          <ul>
+             {
+               this.state.list.map((item,key)=>{
+                 return <li key={key}>{item.name}</li>
+               })
+             }
+          </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
